95b5fd2eafd09285a31390ccbf1f0a07b2232b13
Jarom Allen <jaromallen4@gmail.com>
2021-03-24 11:45:57 -0600

    Added file to average solar data

--- read_in_solar.py
+++ read_in_solar.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-solar_gen = pd.read_csv("solar_generation.csv")
-
-fig = go.Figure(go.Scatter(x=solar_gen['TIME'], y=solar_gen['Meter_value'], name='Solar Generation'))
-fig.update_layout(title='Solar Generation', showlegend=True)
-fig.write_image("fig1.jpg")
+solar_gen = pd.read_csv("new_processed_solar.csv")
+fig = go.Figure(go.Scatter(x=solar_gen['TIME'], y=solar_gen['Solar_Power'], name='Solar Generation'))
+fig.update_layout(title='Averaged Solar Generation', showlegend=True)
+fig.write_image("avg_solar_gen.jpg")
--- read_in_weather.py
+++ read_in_weather.py
@@ -3,6 +3,9 @@ import plotly.graph_objects as go
 
 solar_gen = pd.read_csv("weather_data.csv")
 
-fig = go.Figure(go.Scatter(x=solar_gen['date_time'], y=solar_gen['rso'], name='Solar Radiation'))
+solar_gen2 = solar_gen[0:100]
+fig = go.Figure(go.Scatter(x=solar_gen2['date_time'], y=solar_gen2['rso'], name='Solar Radiation'))
 fig.update_layout(title='Solar Radiation', showlegend=True)
-fig.write_image("fig2.jpg")
+fig.write_image("fig3.jpg")
+print(solar_gen.shape)
+print(solar_gen2.shape)
