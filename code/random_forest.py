6592c54fc4e0fbcafa00077c60eb06cd73d97ad9
Jarom Allen <jaromallen4@gmail.com>
2021-03-29 16:56:46 -0600

    rf stuff

--- code/random_forest.py
+++ code/random_forest.py
@@ -14,11 +14,11 @@ x = np.nan_to_num(x, copy=True)
 y = np.nan_to_num(y, copy=True)
 y = y/1000
 
-x_train = x[0:3999]
-x_test = x[4000:4400]
+x_train = x[0:2999]
+x_test = x[3000:3300]
 
-y_train = y[0:3999]
-y_test = y[4000:4400]
+y_train = y[0:2999]
+y_test = y[3000:3300]
 
 print(y_test.shape)
 
@@ -28,10 +28,9 @@ with np.errstate(invalid='ignore'):
     regressor = RandomForestRegressor(n_estimators=100, random_state=0)
     regressor.fit(x_train, y_train)
     predictions = regressor.predict(x_train)
-    correlation = np.corrcoef(y_train, predictions)
+    #correlation = np.corrcoef(y_train, predictions)
 
-
-    title_name = 'Actual vs Predicted Solar Generation (Avg, RF Training) Corr: {}'.format(correlation[0])
+    title_name = 'Actual vs Predicted Solar Generation (Avg, RF Training)'
     x_axis_label = "Real (KW)"
     y_axis_label = "Predicted (KW)"
 
@@ -40,12 +39,13 @@ with np.errstate(invalid='ignore'):
     plt.xlabel(x_axis_label)
     plt.ylabel(y_axis_label)
     plt.savefig('../figures/rf_train1.jpg')
+    plt.clf()
 
     # TEST DATA
     predictions2 = regressor.predict(x_test)
-    correlation2 = np.corrcoef(y_test, predictions2)
+   # correlation2 = np.corrcoef(y_test, predictions2)
 
-    title_name2 = "Actual vs Predicted Solar Generation (Avg, RF Test) Corr: {}".format(correlation2[0])
+    title_name2 = "Actual vs Predicted Solar Generation (Avg, RF Test)"
     x_axis_label2 = "Real (KW)"
     y_axis_label2 = "Predicted (KW)"
 
