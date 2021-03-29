9a39bd787c90214a88d5d52e95e7031d5118c350
Jarom Allen <jaromallen4@gmail.com>
2021-03-29 11:29:33 -0600

    added train/test split

--- code/random_forest.py
+++ code/random_forest.py
@@ -12,20 +12,46 @@ x = data.to_numpy()
 y = y.to_numpy()
 x = np.nan_to_num(x, copy=True)
 y = np.nan_to_num(y, copy=True)
-y = y/100
+y = y/1000
 
-print(x.shape)
-print(y.shape)
+x_train = x[0:4999]
+x_test = x[5000:-1]
 
-regressor = RandomForestRegressor(n_estimators=100, random_state=0)
-regressor.fit(x, y)
-predictions = regressor.predict(x)
+y_train = y[0:4999]
+y_test = y[5000:-1]
 
-title_name = "Actual vs Predicted Solar Generation (Random Forest)"
-x_axis_label = "Real"
-y_axis_label = "Predicted"
+print(y_test.shape)
 
-plt.scatter(y, predictions)
-plt.savefig('../figures/rf_1.jpg')
+with np.errstate(invalid='ignore'):
 
+    # TRAIN DATA
+    regressor = RandomForestRegressor(n_estimators=100, random_state=0)
+    regressor.fit(x_train, y_train)
+    predictions = regressor.predict(x_train)
+    correlation = np.corrcoef(y_train, predictions)
+
+
+    title_name = 'Actual vs Predicted Solar Generation (RF Training) Corr: {}'.format(correlation)
+    x_axis_label = "Real (KW)"
+    y_axis_label = "Predicted (KW)"
+
+    plt.scatter(y_train, predictions)
+    plt.title(title_name)
+    plt.xlabel(x_axis_label)
+    plt.ylabel(y_axis_label)
+    plt.savefig('../figures/rf_train1.jpg')
+
+    # TEST DATA
+    predictions2 = regressor.predict(x_test)
+    correlation2 = np.corrcoef(y_test, predictions2)
+
+    title_name2 = "Actual vs Predicted Solar Generation (RF Test) Corr: {}".format(correlation2)
+    x_axis_label2 = "Real (KW)"
+    y_axis_label2 = "Predicted (KW)"
+
+    plt.scatter(y_test, predictions2)
+    plt.title(title_name2)
+    plt.xlabel(x_axis_label2)
+    plt.ylabel(y_axis_label2)
+    plt.savefig('../figures/rf_test1.jpg')
 
