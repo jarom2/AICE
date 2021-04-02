520152e861334cd36648a618c81adfad453089de
Jarom Allen <jaromallen4@gmail.com>
2021-04-02 11:41:32 -0600

    one more time

--- avgFig/image.py
+++ avgFig/image.py
@@ -9,3 +9,16 @@ avg_C = np.load("avg_C.npy")
 img_4 = im.fromarray(avg_4*255)
 img_4 = img_4.convert("L")
 img_4.save("img_4.jpg")
+
+img_10 = im.fromarray(avg_10*255)
+img_10 = img_10.convert("L")
+img_10.save("img_10.jpg")
+
+img_32 = im.fromarray(avg_32*255)
+img_32 = img_32.convert("L")
+img_32.save("img_32.jpg")  
+
+img_C = im.fromarray(avg_C*255)
+img_C = img_C.convert("L")
+img_C.save("img_C.jpg")  
+
