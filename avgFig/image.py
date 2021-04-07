0a1a6bace1acad65bfac0dbfcac2191cb63aa3d9
Jarom Allen <jaromallen4@gmail.com>
2021-04-07 13:29:47 -0600

    redid all images

--- avgFig/image.py
+++ avgFig/image.py
@@ -1,10 +1,15 @@
 from PIL import Image as im
 import numpy as np
 
+
 avg_4 = np.load("avg_4.npy")
 avg_10 = np.load("avg_10.npy")
 avg_32 = np.load("avg_32.npy")
 avg_C = np.load("avg_C.npy")
+avg_sm2 = np.load("avg_sm2.npy")
+avg_sm3 = np.load("avg_sm3.npy")
+avg_sm4 = np.load("avg_sm4.npy")
+avg_all = np.load("avg_all.npy")
 
 img_4 = im.fromarray(avg_4*255)
 img_4 = img_4.convert("L")
@@ -16,9 +21,25 @@ img_10.save("img_10.jpg")
 
 img_32 = im.fromarray(avg_32*255)
 img_32 = img_32.convert("L")
-img_32.save("img_32.jpg")  
+img_32.save("img_32.jpg")
 
 img_C = im.fromarray(avg_C*255)
 img_C = img_C.convert("L")
-img_C.save("img_C.jpg")  
+img_C.save("img_C.jpg")
+
+img_sm2 = im.fromarray(avg_sm2*255)
+img_sm2 = img_sm2.convert("L")
+img_sm2.save("img_sm2.jpg")
+
+img_sm3 = im.fromarray(avg_sm3*255)
+img_sm3 = img_sm3.convert("L")
+img_sm3.save("img_sm3.jpg")
+
+img_sm4 = im.fromarray(avg_sm4*255)
+img_sm4 = img_sm4.convert("L")
+img_sm4.save("img_sm4.jpg")  
+
+img_all = im.fromarray(avg_all*255)
+img_all = img_all.convert("L")
+img_all.save("img_all.jpg")  
 
